a58ee829109d1006031acb26f8ccc07ea6a86088
Lindsay Trenton <lt353@hawaii.edu>
2025-04-22 12:02:17 -1000

    put in zybooks code and also adjusted CSS a bit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,68 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 function App() {
+  const [forecast, setForecast] = useState([]);
+  const [zip, setZip] = useState("90210");
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    setZip(event.target.zip.value);
+  }
+
+  useEffect(() => {
+    async function fetchForecast() {
+      setForecast([]);
+      setIsLoading(true);
+      setErrorMessage("");
+
+      const url = `https://wp.zybooks.com/weather.php?zip=${zip}`;
+      const response = await fetch(url);
+      if (response.ok) {
+        const result = await response.json();
+        if (result.success) {
+          setForecast(result.forecast);
+        }
+        else {
+          setErrorMessage(result.error);
+        }
+
+          setIsLoading(false);
+        }
+      }
+  
+      fetchForecast();
+    }, [zip]);
+
   return (
-    <div>
-      <h1>Hello World!</h1>
-      <p>This is a simple template react app.</p>
-      <p>Feel free to modify it as you like.</p>
-      <p>Happy coding!</p>
-    </div>
+    <>
+      <h1>Five Day Weather Forecast for {zip}</h1>
+      <form onSubmit={handleSubmit}>
+        <input 
+          type="number"
+          id="zip"
+          min="11111"
+          max="99999"
+        />
+
+        {errorMessage.length > 0 && <p>{errorMessage}</p>}
+
+        {isLoading ? (
+          <p>Loading...</p>
+        ) : (
+          <ol>
+            {forecast.map((day, index) => (
+              <li key={index}>
+                High: {day.high},
+                Low: {day.low},
+                Desc: {day.desc}
+              </li>
+            ))}
+          </ol>
+        )}
+      </form>
+    </>
   );
 }
 
